4b8dd8f91eb416d5b2a92a16dc06650ddaa127b8
William <157686898+xEdgeLordx@users.noreply.github.com>
2024-05-24 13:43:28 +0200

    Modelyourself
    First commit

---
+++ ModelYourself/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace ModelYourself
+{
+    internal class Person
+    {
+        private string name;
+
+        private int age;
+
+        private string hårfarge;
+
+        private string hobby;
+
+        public Person(string name, int age, string hårfarge, string hobby)
+        {
+            this.name = name;
+            this.age = age;
+            this.hårfarge = hårfarge;
+            this.hobby = hobby;
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine("Hei jeg heter " + name + "\nJeg er " + Convert.ToString(age) + " år gammel\nJeg har " + hårfarge + " hår og nå " + hobby);
+        }
+    } 
+    
+}
---
+++ ModelYourself/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelYourself
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ørjan", 34, "Rødt", "Nå spiser jeg sjelene til syndere.");
+            person.printInfo();
+        }
+    }
+}
